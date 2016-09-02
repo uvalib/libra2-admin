b831635b8b96e985104652719c2539cb40238bb9
UVADave <dpg3k@virginia.edu>
2016-09-02 12:07:43 -0400

    Use correct endpoint for deleting filesets

--- app/models/work_file.rb
+++ app/models/work_file.rb
@@ -1,7 +1,7 @@
 class WorkFile
 	def self.destroy(user, id)
-		#DELETE: http://service.endpoint/api/v1/fileset/:id/?auth=token&user=user
-		status, response = Libra2::api("DELETE", "fileset/#{id}", { user: user })
+		#DELETE: http://service.endpoint/api/v1/filesets/:id/?auth=token&user=user
+		status, response = Libra2::api("DELETE", "filesets/#{id}", { user: user })
 
 		if status
 			return nil
