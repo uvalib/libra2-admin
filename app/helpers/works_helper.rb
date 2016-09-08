d9520c8f1fd6892c9e6cbe509bcac90f09abc988
UVADave <dpg3k@virginia.edu>
2016-09-08 11:13:51 -0400

    Return empty date if we cannot format it correctly

--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -1,11 +1,11 @@
 module WorksHelper
 
   def formatted_date( date )
-     return 'unknown' if date.blank?
+     return '' if date.blank?
      if match = date.match( /^(\d{4}-\d{2}-\d{2}).*$/ )
         return match.captures[ 0 ]
      end
-     return 'unknown'
+     return ''
   end
 
 end
