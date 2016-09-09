835196a3fcb920ae54aa053d375f2d5835e552c5
UVADave <dpg3k@virginia.edu>
2016-09-09 16:33:31 -0400

    More field support

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,126 +1,130 @@
 class Work
-	EDITABLE = [
-		'author_email',
-		'author_first_name',
-		'author_last_name',
-		'title',
-		'abstract',
-		'embargo_state',
-		'embargo_end_date',
-		'notes',
-		'admin_notes',
-		'rights',
-    'language',
-    'keywords',
-    'related_links',
-    'sponsoring_agency',
-#		'advisers',
-	]
-	# 	"advisers:["dpg3k\r\nDavid\r\nGoldstein\r\nUniversity of Virginia Library\r\nUniversity of Virginia"]
+   EDITABLE = [
+         'abstract',
+         'admin_notes',
+         'advisers',
+         'author_email',
+         'author_first_name',
+         'author_last_name',
+         'embargo_end_date',
+         'embargo_state',
+         'degree',
+         'notes',
+         'rights',
+         'title',
+         'author_department',
+         'author_institution',
+         'depositor_email',
+         'keywords',
+         'language',
+         'related_links',
+         'sponsoring_agency',
+   ]
+   #    "advisers:["dpg3k\r\nDavid\r\nGoldstein\r\nUniversity of Virginia Library\r\nUniversity of Virginia"]
 # advisers         - An array of advisers, each being a CRLF separated list of computingId, first name, last name, department, institution (optional)
 
-	EDITABLE_TYPE = {
-		'abstract' => 'textarea',
-		'notes' => 'textarea',
-		'admin_notes' => 'textarea-append',
-		'embargo_state' => 'combo',
-		'rights' => 'combo',
-		'embargo_end_date' => 'date',
-  	'keywords' => 'textarea-split',
-		'related_links' => 'textarea-split',
-		'sponsoring_agency' => 'textarea-split',
-#		'advisers' => 'textarea-split',
-	}
-	RIGHTS = [
-		'CC-BY (permitting free use with proper attribution)',
-		'CC0 (permitting unconditional free use, with or without attribution)',
-		'None (users must comply with ordinary copyright law)'
-	]
-	EMBARGO_STATE = [
-		{ text: 'No Embargo', value: 'open' },
-		{ text: 'UVA Only Embargo', value: 'authenticated' },
-		{ text: 'Metadata Only Embargo', value: 'restricted' }
-	]
+   EDITABLE_TYPE = {
+      'abstract' => 'textarea',
+      'admin_notes' => 'textarea-append',
+      'advisers' => 'textarea-split',
+      'embargo_end_date' => 'date',
+      'embargo_state' => 'combo',
+      'keywords' => 'textarea-split',
+      'notes' => 'textarea',
+      'related_links' => 'textarea-split',
+      'rights' => 'combo',
+      'sponsoring_agency' => 'textarea-split',
+   }
+   RIGHTS = [
+      'CC-BY (permitting free use with proper attribution)',
+      'CC0 (permitting unconditional free use, with or without attribution)',
+      'None (users must comply with ordinary copyright law)'
+   ]
+   EMBARGO_STATE = [
+      { text: 'No Embargo', value: 'open' },
+      { text: 'UVA Only Embargo', value: 'authenticated' },
+      { text: 'Metadata Only Embargo', value: 'restricted' }
+   ]
 
-	def self.all
-		status, response = Libra2::api("GET", 'works')
-		if Libra2::status_ok? status
-			return response['works']
-		else
-			Rails.logger.error "==> Work.all: returns #{status} (#{response})"
-			return []
-		end
-	end
+   def self.all
+      status, response = Libra2::api("GET", 'works')
+      if Libra2::status_ok? status
+         return response['works']
+      else
+         Rails.logger.error "==> Work.all: returns #{status} (#{response})"
+         return []
+      end
+   end
 
-	def self.find(id)
-		status, response = Libra2::api("GET", "works/#{id}")
-		if Libra2::status_ok? status
-			if response['works'] && response['works'].length > 0
-				work = response['works'][0]
-				# The date is received as a full date and time, instead of just a day.
-				work['embargo_end_date'] = work['embargo_end_date'].split("T")[0] if work['embargo_end_date'].present?
+   def self.find(id)
+      status, response = Libra2::api("GET", "works/#{id}")
+      if Libra2::status_ok? status
+         if response['works'] && response['works'].length > 0
+            work = response['works'][0]
+            # The date is received as a full date and time, instead of just a day.
+            work['embargo_end_date'] = work['embargo_end_date'].split("T")[0] if work['embargo_end_date'].present?
 
-				return work
-			else
-				return {}
-			end
-		else
-			Rails.logger.error "==> Work.find: returns #{status} (#{response})"
-			return {}
-		end
-	end
+            return work
+         else
+            return {}
+         end
+      else
+         Rails.logger.error "==> Work.find: returns #{status} (#{response})"
+         return {}
+      end
+   end
 
   def self.latest
-		return search( { status: 'pending', limit: 100 } )
-	end
+      return search( { status: 'pending', limit: 100 } )
+   end
 
   def self.draft
-		return search( { status: 'pending' } )
-	end
+      return search( { status: 'pending' } )
+   end
 
-	def self.submitted
-		return search( { status: 'submitted' } )
-	end
+   def self.submitted
+      return search( { status: 'submitted' } )
+   end
 
-	def self.search(params)
-		status, response = Libra2::api("GET", "works/search", params)
-		if Libra2::status_ok? status
-			return response['works']
-		else
-			Rails.logger.error "==> Work.search: returns #{status} (#{response})"
-			return {}
-		end
-	end
+   def self.search(params)
+      status, response = Libra2::api("GET", "works/search", params)
+      if Libra2::status_ok? status
+         return response['works']
+      else
+         Rails.logger.error "==> Work.search: returns #{status} (#{response})"
+         return {}
+      end
+   end
 
-	def self.update(user, id, params)
-		# PUT: http://service.endpoint/api/v1/works/:id?auth=token&user=user
-		p = { "work" => {} }
-		Work::EDITABLE.each { |field|
-			if params[field]
-				p["work"][field] = params[field]
-				p["work"][field] = [ p["work"][field] ] if field == "admin_notes" # this item requires an array passed to it.
-			end
-		}
-		status, response = Libra2::api("PUT", "works/#{id}", { user: user }, p)
+   def self.update(user, id, params)
+      # PUT: http://service.endpoint/api/v1/works/:id?auth=token&user=user
+      p = { "work" => {} }
+      Work::EDITABLE.each { |field|
+         if params[field]
+            p["work"][field] = params[field]
+            p["work"][field] = [ p["work"][field] ] if field == "admin_notes" # this item requires an array passed to it.
+         end
+      }
+      status, response = Libra2::api("PUT", "works/#{id}", { user: user }, p)
 
-		if Libra2::status_ok? status
-			Rails.logger.error "==> Work.update: returns #{status} (#{response})"
-			return nil
-		else
-			return response
-		end
-	end
+      if Libra2::status_ok? status
+         Rails.logger.error "==> Work.update: returns #{status} (#{response})"
+         return nil
+      else
+         return response
+      end
+   end
 
-	def self.destroy(user, id)
-		# DELETE: http://service.endpoint/api/v1/works/:id?auth=token&user=user
-		status, response = Libra2::api("DELETE", "works/#{id}", { user: user })
+   def self.destroy(user, id)
+      # DELETE: http://service.endpoint/api/v1/works/:id?auth=token&user=user
+      status, response = Libra2::api("DELETE", "works/#{id}", { user: user })
 
-		if Libra2::status_ok? status
-			Rails.logger.error "==> Work.destroy: returns #{status} (#{response})"
-			return nil
-		else
-			return response
-		end
-	end
+      if Libra2::status_ok? status
+         Rails.logger.error "==> Work.destroy: returns #{status} (#{response})"
+         return nil
+      else
+         return response
+      end
+   end
 
 end
