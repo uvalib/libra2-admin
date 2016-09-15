99b9735a6bcba0295a54103d77ac53facf07b860
UVADave <dpg3k@virginia.edu>
2016-09-15 13:43:06 -0400

    Extract time for display if we can

--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -2,10 +2,17 @@ module WorksHelper
 
   def formatted_date( date )
      return '' if date.blank?
+     d = ''
+     t = ''
      if match = date.match( /^(\d{4}-\d{2}-\d{2}).*$/ )
-        return match.captures[ 0 ]
+        d = match.captures[ 0 ]
      end
-     return ''
+
+     if match = date.match( /(\d{2}:\d{2}:\d{2}).*$/ )
+       t = match.captures[ 0 ]
+     end
+
+     return "#{d} #{t}"
   end
 
 end
