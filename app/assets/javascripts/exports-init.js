2ca98ae5bbb6138aa8e168db3d226a13c2a00a5b
UVADave <dpg3k@virginia.edu>
2016-10-14 15:24:55 -0400

    LIBRA-601: download of csv files

--- app/assets/javascripts/exports-init.js
+++ app/assets/javascripts/exports-init.js
@@ -1,5 +1,6 @@
 (function() {
 	"use strict";
+
 	function initPage() {
 
         $('.data-table.exports-index').DataTable({
@@ -19,12 +20,35 @@
             dateFormat: "yy-mm-dd"
         });
 
-        $("#work-export").on("click", function(ev) {
-
+        // export handlers
+        $("#work-export").on("click", function( ev ) {
             ev.preventDefault();
-            //alert( "click" );
+            var start_date = getDateOrToday( "work-export-start-date" );
+            var end_date = getDateOrToday( "work-export-start-date");
+            getExports( "works", start_date, end_date );
+        });
+        $("#fileset-export").on("click", function( ev ) {
+            ev.preventDefault();
+            var start_date = getDateOrToday( "fileset-export-start-date" );
+            var end_date = getDateOrToday( "fileset-export-start-date");
+            getExports( "filesets", start_date, end_date );
+        });
+        $("#audit-export").on("click", function( ev ) {
+            ev.preventDefault();
+            var start_date = getDateOrToday( "audit-export-start-date" );
+            var end_date = getDateOrToday( "audit-export-start-date");
+            getExports( "audit", start_date, end_date );
         });
 	}
+
+	function getExports( export_type, start_date, end_date ) {
+        var url = $("#api-endpoint" ).val() + "/" + export_type + ".csv?auth=" + $("#api-token" ).val();
+        window.location = url;
+    }
+
+    function getDateOrToday( field_id ) {
+       return "2016-01-01"
+    }
 
 	document.addEventListener("turbolinks:load", function() {
 		initPage();
