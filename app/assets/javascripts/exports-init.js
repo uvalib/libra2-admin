a872be2674fc8e8bc1f16e7d1c14b1f26f7091b2
UVADave <dpg3k@virginia.edu>
2016-10-17 09:00:59 -0400

    LIBRA-601: pass date fields in REST query as necessary

--- app/assets/javascripts/exports-init.js
+++ app/assets/javascripts/exports-init.js
@@ -24,30 +24,36 @@
         $("#work-export").on("click", function( ev ) {
             ev.preventDefault();
             var start_date = getDateOrToday( "work-export-start-date" );
-            var end_date = getDateOrToday( "work-export-start-date");
+            var end_date = getDateOrToday( "work-export-end-date");
             getExports( "works", start_date, end_date );
         });
         $("#fileset-export").on("click", function( ev ) {
             ev.preventDefault();
             var start_date = getDateOrToday( "fileset-export-start-date" );
-            var end_date = getDateOrToday( "fileset-export-start-date");
+            var end_date = getDateOrToday( "fileset-export-end-date");
             getExports( "filesets", start_date, end_date );
         });
         $("#audit-export").on("click", function( ev ) {
             ev.preventDefault();
             var start_date = getDateOrToday( "audit-export-start-date" );
-            var end_date = getDateOrToday( "audit-export-start-date");
+            var end_date = getDateOrToday( "audit-export-end-date");
             getExports( "audit", start_date, end_date );
         });
 	}
 
 	function getExports( export_type, start_date, end_date ) {
-        var url = $("#api-endpoint" ).val() + "/" + export_type + ".csv?auth=" + $("#api-token" ).val();
+        var url = $("#api-endpoint" ).val() + "/" + export_type + ".csv?auth=" + $("#api-token" ).val() +
+            "&create_date=" + start_date + ":" + end_date;
+        console.log( "export URL: " + url );
         window.location = url;
     }
 
     function getDateOrToday( field_id ) {
-       return "2016-01-01"
+        var date = $( "#" + field_id ).val( );
+        if ( date.length === 0 ) {
+            date = new Date( ).toISOString( ).slice( 0, 10 );
+        }
+        return date
     }
 
 	document.addEventListener("turbolinks:load", function() {
