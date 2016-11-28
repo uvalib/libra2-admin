ad9b1b6c359d3a30ac97f5991c4076387d369760
UVADave <dpg3k@virginia.edu>
2016-11-28 15:09:48 -0500

    LIBRA-639: remove special characters from the template file label (part2)

--- app/models/work.rb
+++ app/models/work.rb
@@ -268,7 +268,7 @@ class Work
 
   def self.label_filter( label )
      return '' if label.blank?
-     return label.gsub( /[^0-9,a-z,A-Z,-,_]/, '' )
+     return label.gsub( /[^0-9a-zA-Z-_]/, '' )
   end
 
   def self.get_options( which )
