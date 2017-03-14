558479cd988eb02896825a4c05ad79d0aa9ca282
UVADave <dpg3k@virginia.edu>
2017-03-14 14:38:50 -0400

    LIBRA-706: ability to export all works instead of just submitted ones

--- app/assets/javascripts/exports-init.js
+++ app/assets/javascripts/exports-init.js
@@ -23,27 +23,37 @@
         // export handlers
         $("#work-export").on("click", function( ev ) {
             ev.preventDefault();
-            var start_date = getDateOrToday( "work-export-start-date" );
+            var include_pending = $("#include_pending").is(':checked');
+            alert( include_pending );
+            var start_date = getDateOrBeginingOfTime( "work-export-start-date" );
             var end_date = getDateOrToday( "work-export-end-date");
-            getExports( "works/search", start_date, end_date );
+            var constraints = "create_date=" + start_date + ":" + end_date;
+            if( include_pending === false ) {
+                constraints = constraints + "&status=submitted";
+            }
+            getExports( "works/search", constraints );
         });
+
         $("#fileset-export").on("click", function( ev ) {
             ev.preventDefault();
-            var start_date = getDateOrToday( "fileset-export-start-date" );
+            var start_date = getDateOrBeginingOfTime( "fileset-export-start-date" );
             var end_date = getDateOrToday( "fileset-export-end-date");
-            getExports( "filesets", start_date, end_date );
+            var constraints = "create_date=" + start_date + ":" + end_date;
+            getExports( "filesets", constraints );
         });
+
         $("#audit-export").on("click", function( ev ) {
             ev.preventDefault();
-            var start_date = getDateOrToday( "audit-export-start-date" );
+            var start_date = getDateOrBeginingOfTime( "audit-export-start-date" );
             var end_date = getDateOrToday( "audit-export-end-date");
-            getExports( "audit", start_date, end_date );
+            var constraints = "create_date=" + start_date + ":" + end_date;
+            getExports( "audit", constraints );
         });
 	}
 
-	function getExports( export_type, start_date, end_date ) {
+	function getExports( export_type, constraints ) {
         var url = $("#api-endpoint" ).val() + "/" + export_type + ".csv?auth=" + $("#api-token" ).val() +
-            "&create_date=" + start_date + ":" + end_date;
+            "&" + constraints;
         console.log( "export URL: " + url );
         window.location = url;
     }
@@ -52,6 +62,14 @@
         var date = $( "#" + field_id ).val( );
         if ( date.length === 0 ) {
             date = new Date( ).toISOString( ).slice( 0, 10 );
+        }
+        return date
+    }
+
+    function getDateOrBeginingOfTime( field_id ) {
+        var date = $( "#" + field_id ).val( );
+        if ( date.length === 0 ) {
+            date = '2000-01-01';
         }
         return date
     }
